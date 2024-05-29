f11bbbbaafe5f3798e236d7aecc6ba504fac3e2a
vrcalomino <vrcalomino@gmail.com>
2024-05-28 21:27:38 -0300

    modified style

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 /* eslint-disable react/prop-types */
 import { useState } from 'react'
 import { useId } from 'react'
+import "./App.css"
 
 const Task = ({id, name, removeTask}) => {
   
   return (
-  <li id={id}>
-    {name}
-    <button onClick={() => removeTask(id)}>-</button>
+  <li className="task" id={id}>
+    <span className='task-name'>{name}</span>
+    <button className='task-remove' onClick={() => removeTask(id)}>-</button>
   </li>)
 }
 
 
 function App() {
   const [newTaskName, setNewTaskName] = useState("")
-  const date = new Date().toLocaleDateString().slice(0, 4)
+  const date = new Date().toLocaleDateString().slice(0, 4).split("/").reverse().join("/")
   const [tasks, setTasks] = useState([])
   const id = useId()
 
@@ -24,6 +25,7 @@ function App() {
     event.preventDefault()
     const newTask = { id: id + tasks.length, name: newTaskName };
     setTasks([...tasks, newTask]);
+    setNewTaskName("")
   };
 
   const removeTask = (id) => {
@@ -33,12 +35,12 @@ function App() {
   return (
     <>
       <div className='task-box'>
-        <h1>{date}</h1>
-        <ul>
+        <h1>Tareas del dia {date}</h1>
+        <ul className='task-list'>
           {tasks.map(task => (
-            <Task id={task.id} key={task.id} name={task.name} removeTask={removeTask} />
+            <Task  id={task.id} key={task.id} name={task.name} removeTask={removeTask} />
           ))}
-          <form onSubmit={(event) => addTask(event)}>
+          <form className='task-add' onSubmit={(event) => addTask(event)}>
             <input value={newTaskName} onChange={(event) => setNewTaskName(event.target.value)} />
             <button type='submit'>+</button>
           </form>
